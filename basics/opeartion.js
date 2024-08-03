5ae3b80cbd8e9f9593ab10adba452f83f32c6b56
mukund302002 <mukundagarwalla2002@gmail.com>
2024-08-03 12:08:03 +0530

    operations

---
+++ basics/opeartion.js
@@ -0,0 +1,28 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+
+// iss type ke conversions me agar string wala vaue pehle aa rha hai toh baaki ka saara string count hoga
+// ans agar string se pehle koi integer hai toh vo pehle mathematical operation krega fir string ho jayega
+// inshort jaha string aaya uske baad sab string ban jayega.
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
