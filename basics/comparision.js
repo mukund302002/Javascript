5b419650a1eb7cc8d6b5cb8adb3023dc5cd95c89
mukund302002 <mukundagarwalla2002@gmail.com>
2024-08-03 12:15:01 +0530

    comparing different datatypes

---
+++ basics/comparision.js
@@ -0,0 +1,23 @@
+// COMPARE KRTE TIME VALUE CONVERT HONE LAGTA HAI
+
+
+console.log("2" > 1);
+// isme string convert ho gya interger me
+
+
+// yaha null converted to 0
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// here all false
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// == sirf avlue ko hi check krta hai
+
+// === value bhi check krta hai as well as datatype bhi check krta hai.
+
+console.log("2"<'1');
