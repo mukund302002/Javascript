570c89b6de6af34390e90030703d1a9ea23c5054
mukund302002 <mukundagarwalla2002@gmail.com>
2024-08-03 11:35:11 +0530

    first commit

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+let name= "mukund"
+let age= 22
+let isbool = false
+
+//js me mention nahi krna padta ki konsa datatype ak value daal rhe hai varible ke andar agar let use krr rhe toh.
+
+let state = null 
+// null ka mtlb undefined nahi hai, null mtlb abhi vo khali hai
+let stat
+//and undifined mtlb the value is not yet defined
+
+// finding the datatype of any variables.
+console.log(typeof name)
---
+++ basics/variables.js
@@ -0,0 +1,21 @@
+//we can not change the const variable
+const accountId= 1138
+
+let name="Mukund"
+//mostly we use let in todays programming
+
+var number= 121212
+// var is not used in todays world
+
+surname="agarwalla"
+// this is very bad way of declaring
+
+let id
+// the default value of any variable wwithout assignment is undefined
+
+
+name="Himanshu"
+number= 212121
+surname="kumar"
+
+console.table([name, number, surname,id])
